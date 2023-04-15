ecd729aa2ed873275e22bab8350114d1ad032d71
ishimwepatrick <mailsxender@gmail.com>
2023-04-15 10:51:23 +0200

    first commit

---
+++ static/js/39.02b7be2e.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpiCountApp=this.webpackJsonpiCountApp||[]).push([[39],{528:function(s,e,t){"use strict";t.r(e);t(0);var a=t(2);e.default=({name:s,desc:e})=>Object(a.jsxs)("div",{className:"mb-3",children:[Object(a.jsxs)("span",{className:"font-weight-bold text-dark",children:[s,":"]}),Object(a.jsxs)("span",{className:"text-gray-light",children:[" ",e]})]})}}]);
+//# sourceMappingURL=39.02b7be2e.chunk.js.map
